60dd856c5a12a7a7bd3d7e37b76350401a923555
bardia <zarabadybardya@gmail.com>
2026-07-27 11:53:33 +0330

    init: project structure

---
+++ Downloads/smart ATS/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
